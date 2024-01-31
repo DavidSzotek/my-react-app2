741c2bd50a0ca746479428442f9ba9fb3943fbb4
David Szotek <david.szotek.vs@gmail.com>
2024-01-31 16:07:06 +0100

    Different style connections commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import Card from "./Card.jsx";
+import Button from "./Button.jsx";
+import Button_modular from "./Button_modular/Button_modular.jsx";
+import Button_inline from "./Button_inline.jsx";
+
+/* External - can have a lot naming conflicts */
+/* Modular */
+/* Inline style - useful small isolated components with little styling */
 
 function App() {
   return (
@@ -7,6 +14,9 @@ function App() {
       <Card />
       <Card />
       <Card />
+      <Button />
+      <Button_modular />
+      <Button_inline />
     </>
   );
 }
---
+++ src/Button_modular/Button_modular.jsx
@@ -0,0 +1,7 @@
+import styles from "./Button_modular.module.css";
+
+function Button_modular() {
+  return <button className={styles.button_modular}>Click me</button>;
+}
+
+export default Button_modular;
